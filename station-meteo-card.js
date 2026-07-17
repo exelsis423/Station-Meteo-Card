660bf34d4a950177c8acf07092ad293ab291d90f
exelsis423 <119899103+exelsis423@users.noreply.github.com>
2026-07-17 15:59:11 +0200

    Update card styles with border and shadow effects

--- station-meteo-card.js
+++ station-meteo-card.js
@@ -18,8 +18,8 @@ class StationMeteoCard extends LitElement {
     ha-card {
       border-radius: 20px;
       overflow: hidden;
-      border: none !important;
-      box-shadow: none !important;
+      border: none 1px rgba(0,0,0,0.4) outset;
+      box-shadow: 2px 2px 4px 0px rgba(0,0,0,0.5) !important;
     }
 
     .screen {
